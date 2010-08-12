07400a248640d27b3587eead28a2eef4cf39ede5
marano <thiagomarano@gmail.com>
2010-08-12 15:26:42 -0300

    Updated schema, after db:migrate.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100809190559) do
+ActiveRecord::Schema.define(:version => 20100811100000) do
 
   create_table "organizations", :force => true do |t|
     t.string   "name"
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(:version => 20100809190559) do
     t.string   "city"
     t.string   "country"
     t.text     "needs"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "organizations_tags", :id => false, :force => true do |t|
+    t.integer "organization_id"
+    t.integer "tag_id"
+  end
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
