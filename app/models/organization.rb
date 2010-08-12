7d714170234d603728e9bf52f176995f0bd223b4
Duda Dornelles <ddornell@thoughtworks.com>
2010-08-12 10:26:17 -0300

    Duda : fixing association between tags and organizations, it must be 'has_and_belongs_to_may'.

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,6 +3,6 @@ class Organization < ActiveRecord::Base
 	validates_presence_of :name, :responsible, :city, :country
 	validates_uniqueness_of :name
 
-	has_many :tags
+	has_and_belongs_to_many :tags
 
 end
