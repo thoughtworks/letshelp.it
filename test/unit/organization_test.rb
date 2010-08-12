de27c9b951798f90e09e07448c0680e465f52f93
Rodrigo Guilherme Wolschick <rodrigogui@gmail.com>
2010-08-12 15:26:01 -0300

    Lucas/Rodrigo - #1 - Some db tests.

--- test/unit/organization_test.rb
+++ test/unit/organization_test.rb
@@ -5,4 +5,23 @@ class OrganizationTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+
+  test "should not save Organization without name, city, country" do
+    org = Organization.new
+    assert !org.save
+
+    org.name = 'Red Cross'
+    assert !org.save, 'Should not insert with just name'
+
+    org.city = 'Orlando'
+    assert !org.save, 'Should not insert with just name and city'
+
+    org.responsible = 'Rubens Barrichelo'
+    assert !org.save, 'Should not insert with just name, city and responsible'
+
+    org.country = 'United States of America' 
+    assert org.save, 'Should save organization'
+ 
+  end
+
 end
