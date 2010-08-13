a1d0a516935b7825980b1d39f9b32f0cc3532e8c
Duda Dornelles <ddornell@thoughtworks.com>
2010-08-13 10:36:16 -0300

    Duda/Joao #5 : Added uniqueness of tag

--- test/unit/tag_test.rb
+++ test/unit/tag_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class TagTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "should not create two tags with the same name" do
+    tag = Tag.new(:name => "Dilma Caveira")
+    assert tag.save
+    tag = Tag.new(:name => "Dilma Caveira")
+    assert (not tag.save)
   end
+
 end
+
