e26b97a7b50ed2b57abe9799410f962112f56690
Lucas Torri <lucastorri@gmail.com>
2010-08-13 15:47:12 -0300

    Validate presence of name in new Tag.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ActiveRecord::Base
+  
+  validates_presence_of :name
 	validates_uniqueness_of :name
 	has_and_belongs_to_many :organizations
 	
---
+++ spec/models/tag_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe Tag do
+
+  it { should validate_presence_of(:name) }
+
+end
+
