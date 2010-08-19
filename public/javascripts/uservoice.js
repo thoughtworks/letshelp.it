4b4e7f7e25573eaa11f04dc9a034c7df414e9fc7
Daniel Wildt <dwildt@gmail.com>
2010-08-19 11:13:56 -0300

    dwildt #13 - Adding user voice widget to main screen

---
+++ public/javascripts/uservoice.js
@@ -0,0 +1,22 @@
+var uservoiceOptions = {
+  /* required */
+  key: 'letshelpit',
+  host: 'letshelpit.uservoice.com', 
+  forum: '71821',
+  showTab: true,  
+  /* optional */
+  alignment: 'right',
+  background_color:'#331200', 
+  text_color: 'white',
+  hover_color: '#553422',
+  lang: 'pt_BR' /* en */
+};
+
+function _loadUserVoice() {
+  var s = document.createElement('script');
+  s.setAttribute('type', 'text/javascript');
+  s.setAttribute('src', ("https:" == document.location.protocol ? "https://" : "http://") + "cdn.uservoice.com/javascripts/widgets/tab.js");
+  document.getElementsByTagName('head')[0].appendChild(s);
+}
+_loadSuper = window.onload;
+window.onload = (typeof window.onload != 'function') ? _loadUserVoice : function() { _loadSuper(); _loadUserVoice(); };
