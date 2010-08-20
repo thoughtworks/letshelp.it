b1510d85e28a6989f7d624e533addce25b9c9488
rmagrin <rafael.magrin@gmail.com>
2010-08-20 18:51:28 -0300

    Magrin #12 - Added route so the organization page can be accessed using 'http://letshelp.it/organization-id'

--- config/routes.rb
+++ config/routes.rb
@@ -38,6 +38,8 @@ ActionController::Routing::Routes.draw do |map|
 
   # See how all your routes lay out with "rake routes"
 
+  map.organization_shortcut '/:id', :controller => 'organizations', :action => 'show'
+
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
