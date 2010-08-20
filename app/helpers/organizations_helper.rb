fefcefeb76cc3d5fc180827023bda4c7ca6716b3
rmagrin <rafael.magrin@gmail.com>
2010-08-20 14:40:34 -0300

    Magrin #9: Share NGO information with friends using twitter

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -1,7 +1,17 @@
 module OrganizationsHelper
+  require 'uri'
 
 	def has_tag?(tag)
 		not @organization.nil? and @organization.tags.include?(tag)
 	end
+
+  def compose_twitter_share_url()
+     URI.escape('http://twitter.com/share?url=http://letshelp.it' +
+        url_for(@organization) +
+        '&amp;text=' +
+        t("twitter_share.please_help") +
+        @organization.name +
+        '&amp;via=LetsHelpIt')
+  end
 
 end
