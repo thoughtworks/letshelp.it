41614dde316f17f92fea83f6a7d6510f7c29d336
eduaquiles <eduardoaquiles.ar@gmail.com>
2010-08-23 11:28:09 -0300

    Email format will be checked only if not empty when creating new organization

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,7 +2,7 @@ class Organization < ActiveRecord::Base
   has_friendly_id :name, :use_slug => true, :cache_column => 'name_slug', :approximate_ascii => true
 
 	validates_presence_of :name, :contact, :city, :country, :password,  :announcer, :email
-	validates_format_of :email, :with  => /[\w+-][\w.+-]*@\w+\.\w+/, :message => "wrong_mail_address"
+	validates_format_of :email, :with  => /[\w+-][\w.+-]*@\w+\.\w+/, :message => "wrong_mail_address", :if => Proc.new { |organization| !organization.email.empty?}
 	validates_uniqueness_of :name
 
 	has_and_belongs_to_many :tags
