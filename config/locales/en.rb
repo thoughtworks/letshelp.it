f1ab77b5ae79332b0ebe28a0e6a5f0b101590c4b
rmagrin <rafael.magrin@gmail.com>
2010-08-23 11:45:51 -0300

    Magrin #10 - Changed translation files from yaml to rb to fix enconding problems with non-ASCII characters

---
+++ config/locales/en.rb
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+{
+  :en => {
+    :title => "Let's Help It!",
+    :howto_help => "How to help?",
+    :whereto_help => "City, State, Country, Planet, ...",
+    :organization => {
+      :register => "Register your organization"  
+    },
+    :tag => {
+      :food => "Food",
+      :clothes => "Clothes",
+      :volunteer_work => "Volunteer Work",
+      :money => "Money",
+      :others => "Others",
+    },
+    :twitter_share => {
+      :please_help => "Please help ",
+      :button => "Share with Twitter"
+    }
+  }
+}
+
---
+++ config/locales/pt.rb
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+{
+  :pt => {
+    :title => "Let's Help It!",
+    :howto_help => "Como ajudar?",
+    :whereto_help => "Cidade, Estado, País, Planeta, ...",
+    :organization => {
+      :register => "Registre sua organização"
+    },
+    :tag => {
+      :food => "Comida",
+      :clothes => "Roupas",
+      :volunteer_work => "Trabalho Voluntário",
+      :money => "Dinheiro",
+      :others => "Outros"
+    },
+    :twitter_share => {
+      :please_help => "Por favor ajude ",
+      :button => "Compartilhe com Twitter"
+    }
+  }
+}
