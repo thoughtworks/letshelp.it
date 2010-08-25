74251f67482522f4ac09cc3294de6a2f4e74fcb1
Lucas Torri <lucastorri@gmail.com>
2010-08-25 14:11:56 -0300

    Changing text word to should, available on shoulda.

--- test/functional/organizations_controller_test.rb
+++ test/functional/organizations_controller_test.rb
@@ -10,46 +10,46 @@ class OrganizationsControllerTest < ActionController::TestCase
     should_not set_the_flash
   end
 
-  test "should get new" do
+  should "get new" do
     get :new
     assert_response :success
   end
 
-  test "should create organization" do
+  should "create organization" do
     assert_difference('Organization.count') do
-      post :create, :organization => { :name => "title", :contact => "contact", :city => "Jaragua do Sul", :password => "password", :email => "email@mail.com" , :country => "country", :announcer => "announcer" }
+      post :create, :organization => { :name => "title", :contact => "contact", :city => "Jaragua do Sul", :password => "password", :email => "email@mail.com", :country => "country", :announcer => "announcer"}
     end
     assert_redirected_to organization_path(assigns(:organization))
   end
 
-  test "should show organization" do
+  should "show organization" do
     get :show, :id => organizations(:redcross).to_param
     assert_response :success
   end
 
-  test "should get edit" do
+  should "get edit" do
     get :edit, :id => organizations(:wwf).to_param
     assert_response :success
   end
 
-  test "should update organization" do
-    put :update, :id => organizations(:wwf).to_param, :organization => {:name => "updated title" }
+  should "update organization" do
+    put :update, :id => organizations(:wwf).to_param, :organization => {:name => "updated title"}
     assert_redirected_to organization_path(assigns(:organization))
   end
 
-  test "should destroy organization" do
+  should "destroy organization" do
     assert_difference('Organization.count', -1) do
       delete :destroy, :id => organizations(:wwf).to_param
     end
     assert_redirected_to organizations_path
   end
 
-  test "should show tags when there is a validation error while creating a organization" do
+  should "show tags when there is a validation error while creating a organization" do
     put :create, :organization => {}
     assert_response :success 
   end
 
-  test "should reply the organizations related to a name in the search term" do
+  should "reply the organizations related to a name in the search term" do
     get :search, :q => 'Red Cross'
     assert_search_successful [organizations(:redcross)], assigns(:organizations)
     
@@ -60,23 +60,23 @@ class OrganizationsControllerTest < ActionController::TestCase
     assert_search_successful [organizations(:santa_casa)], assigns(:organizations)
   end
   
-  test "should reply the organizations related to a city in the search term" do
+  should "reply the organizations related to a city in the search term" do
     get :search, :q => 'tangamandapio'
     
     assert_search_successful [organizations(:wwf)], assigns(:organizations)
   end
 
-  test "should not reply the organizations related to a tag name in the search term" do
+  should "not reply the organizations related to a tag name in the search term" do
     get :search, :q => 'food'
     assert_search_successful [], assigns(:organizations)
   end
   
-  test "should reply search filtered by the selected tags" do
+  should "reply search filtered by the selected tags" do
     get :search, :q => 'r', :tag_ids => [tags(:cloth).id.to_s]
     assert_search_successful [organizations(:wwf)], assigns(:organizations)
   end
   
-  test "should return all organizations with the given tags" do
+  should "return all organizations with the given tags" do
     get :search, :tag_ids => [tags(:food).id.to_s]
     assert_search_successful [organizations(:redcross), organizations(:wwf), organizations(:xpto)], assigns(:organizations)
     
@@ -84,7 +84,7 @@ class OrganizationsControllerTest < ActionController::TestCase
     assert_search_successful [organizations(:wwf), organizations(:xpto), organizations(:redcross)], assigns(:organizations)
   end
   
-  test "should be able to find cities with and without accent" do
+  should "be able to find cities with and without accent" do
     get :search, :q => 'são paulo'
     assert_search_successful [organizations(:greenpeace), organizations(:santa_casa)], assigns(:organizations)
     
@@ -92,12 +92,12 @@ class OrganizationsControllerTest < ActionController::TestCase
     assert_search_successful [organizations(:greenpeace), organizations(:santa_casa)], assigns(:organizations)
   end
 
-  test "should be able to edit organization when match password" do
+  should "be able to edit organization when match password" do
     get :ajax_edit, :id => organizations(:wwf).id, :password => organizations(:wwf).password
     assert_response :success
   end
   
-  test "should not be able to edit organization when password dont match" do
+  should "not be able to edit organization when password dont match" do
     get :ajax_edit, :id => organizations(:wwf).id, :password => "wrong_pass"
     assert :status => :failure
   end
