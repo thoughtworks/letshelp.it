579df2007fc87cb2d2327e85bebb2157e2279af6
rmagrin <rafael.magrin@gmail.com>
2010-09-05 04:27:59 -0300

    Magrin - moved locale menu html generation to helper method

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,11 @@
 module HomeHelper
+  
+  def generate_locale_menu_html
+    '<div id="locale_menu">' +
+      '<form method="get">' +
+        select_tag("locale", options_for_select(I18n.available_locales.collect{ |x| [I18n.find_locale_name(x), x]}, I18n.locale), { :onchange => "this.form.submit()"}) +
+      '</form>' +
+    '</div>'
+  end
+  
 end
