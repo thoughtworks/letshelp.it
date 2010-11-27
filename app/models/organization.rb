3e6c00d46392d50cb194dd23366e3bd3f8336088
Pedro Nascimento <pnascimento@gmail.com>
2010-11-27 17:56:47 -0200

    removed has_tag? method, we don't need it after all.

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,7 +1,7 @@
 class Organization < ActiveRecord::Base
 
   extend Random
-  
+
   has_friendly_id :name, :use_slug => true, :cache_column => 'name_slug', :approximate_ascii => true
 
   validates_presence_of :name, :contact, :city, :country, :password,  :announcer, :email
@@ -11,26 +11,23 @@ class Organization < ActiveRecord::Base
 
   has_and_belongs_to_many :tags
 
-  def has_tag?(tag)
-    not tags.nil? and tags.include?(tag) 
-  end
-
   def city=(name)
     write_attribute(:city, name)
     write_attribute(:city_slug, Organization.slug_city(name)) if name
   end
-  
+
   def self.slug_name(str)
     str.to_s.to_slug.approximate_ascii.normalize.to_s
   end
-  
+
   def self.slug_city(name)
     name.to_s.to_slug.approximate_ascii.to_s.downcase
   end
-  
+
   def self.get_random_list(limit)
     organizations = Organization.all(:select => :id, :order => db_random, :limit => limit)
     organizations.each { |u| u.reload}
   end
 
 end
+
