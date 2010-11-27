98beb6ca7cc8af797654ed188025afc6a29292eb
Pedro Nascimento <pnascimento@gmail.com>
2010-11-27 17:57:51 -0200

    removed has_tag? method, we don't need it after all.

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -1,10 +1,6 @@
 module OrganizationsHelper
   require 'uri'
 
-  def has_tag?(tag)
-    @organization.tags.include?(tag) if @organization.present?
-  end
-
   def compose_twitter_share_url()
      URI.escape('http://twitter.com/share?url=http://letshelp.it/' +
         @organization.name_slug +
