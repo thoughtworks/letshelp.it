38578bfb340c523d79c6ab85a2285fc47c121602
Daniel Wildt <dwildt@gmail.com>
2011-12-03 17:43:09 -0200

    adding facebook and twitter like button for each organization Closes #42

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -1,14 +1,20 @@
 module OrganizationsHelper
   require 'uri'
 
-  def compose_twitter_share_url()
-     URI.escape('http://twitter.com/share?url=http://letshelp.it/' +
+  def compose_twitter_share_text()
+     'http://letshelp.it/' +
         @organization.name_slug +
-        '&amp;text=' +
+        ' ' +
         t("twitter_share.please_help") +
-        @organization.name +
-        '&amp;via=LetsHelpIt')
+        @organization.name
   end
+
+
+  def compose_facebook_like_url()
+     URI.escape('http://letshelp.it/' +
+        @organization.name_slug)
+  end
+
 
 end
 
